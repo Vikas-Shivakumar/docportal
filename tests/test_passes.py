9cd49d5d4a74c412882ee68d9951934103cfb16a
Vikas S <svikasgowda@gmail.com>
2026-02-26 14:06:07 +0530

    added github actions for ci

--- tests/test_passes.py
+++ tests/test_passes.py
@@ -1,8 +1,14 @@
+
 import yaml
 
 def load_config(config_path="config/config.yaml"):
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
         return config
-    
-load_config()
+
+def test_python_working():
+    assert 1 + 1 == 2
+
+def test_config_loads():
+    config = load_config()
+    assert isinstance(config, dict)
